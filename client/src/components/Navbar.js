561071c2e2a87f1c1840b24306e2282651d293e4
Joshua Hawks <jdhawks@gmail.com>
2022-01-23 22:47:20 -0600

    navbar links & logo import

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Logo from '../assets/Logo.svg';
 //styles
 import './Navbar.css';
 
@@ -9,6 +9,7 @@ function Navbar() {
 		<nav className='navbar'>
 			<ul>
 				<li className='logo'>
+					<img src={Logo} alt='logo' />
 					<Link to='/'>
 						<h1>Joshua Hawks</h1>
 					</Link>
@@ -18,6 +19,24 @@ function Navbar() {
 				</li>
 				<li>
 					<Link to='/about'>About</Link>
+				</li>
+				<li>
+					<a
+						href='https://drive.google.com/file/d/1jbeHE5qYxWmL1CXbRIayN6GthQoK3pv-/view?usp=sharing'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						Resume
+					</a>
+				</li>
+				<li>
+					<a
+						href='https://jdhawks.myportfolio.com/'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						Adobe Portfolio
+					</a>
 				</li>
 
 				<li>
