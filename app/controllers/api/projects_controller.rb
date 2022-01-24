3fd0e1bc2119384fb8c03e16cd9e89811d0281b4
Joshua Hawks <jdhawks@gmail.com>
2022-01-23 20:53:17 -0600

    project controller, backend routes, and table migration

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,2 +1,17 @@
 class Api::ProjectsController < ApplicationController
+before_action :set_project, only: [:show]
+
+  def index
+    render json: Project.all, status: :ok
+  end
+
+  def show
+    render json: @project, status: :ok
+  end
+
+  private
+
+  def set_project
+		@project = Project.find(params[:id])
+	end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::API
 	include ActionController::Cookies
+	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+	rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
 
 	def hello_world
 		session[:count] = (session[:count] || 0) + 1
 		render json: { count: session[:count] }
+	end
+
+	private
+	def record_not_found(errors)
+		render json: errors.message, status: :not_found
+	end
+
+	def invalid_record(invalid)
+		render json: invalid.record.errors, status: :unprocessable_entity
 	end
 end
