c6bc494cfbeb1902c440b5f912b5f32abdbad1d3
Joshua Hawks <jdhawks@gmail.com>
2022-02-03 09:01:11 -0600

    connect buttons functioning

--- client/src/pages/contact/Contact.js
+++ client/src/pages/contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Connect from '../../assets/connect.svg';
+import LinkedIn from '../../assets/linkedin.svg';
+import GitHub from '../../assets/github.svg';
+import Email from '../../assets/email.svg';
 
 //styles
 import './Contact.scss';
@@ -12,7 +15,27 @@ function Contact() {
 				<h1 className='secondary'>Joshua Hawks</h1>
 				<h2 className='tertiary'>Full-Stack Developer</h2>
 				<div className='connect-btns'>
-					<img src='' alt='' />
+					<a
+						href='https://www.linkedin.com/in/joshuahawks1/'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						<img src={LinkedIn} alt='linkedin button' />
+					</a>
+					<a
+						href='https://github.com/jdhawks2132'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						<img src={GitHub} alt='github button' />
+					</a>
+					<a
+						href='mailto: jdhawks@gmail.com'
+						target='_blank'
+						rel='noreferrer noopener'
+					>
+						<img src={Email} alt='email button' />
+					</a>
 				</div>
 			</div>
 		</div>
